6fad86a746c90b828f36d0a8e6c79169726022b4
aryanjiyani <aryanjiyani19@gmail.com>
2022-09-04 11:48:29 +0530

    deploy

--- test/helloworld.ts
+++ test/helloworld.ts
@@ -2,19 +2,25 @@ import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 import { expect } from "chai";
 
-describe("hello world", function() {
+// describe("Hello world", () => {
+//     it("should get the hello world", async () => {
+//         const HW = await ethers.getContractFactory("HelloWorld");
+//         const hello = await HW.deploy();
+
+//         expect(await hello.hello()).to.equal("Hello, World");
+//     });
+// });
+
+ describe("hello world", function() {
     it("should say hi", async function() {
         // 1. setup env.
         // 2. deploy our contract
         // 3. call our functions to test
-
         // 2)
         const HelloWorld = await ethers.getContractFactory("HelloWorld");
         const hello = await HelloWorld.deploy();
         await hello.deployed();
 
         expect(await hello.hello()).to.equal("Hello, World");
-
-
     });
 });
