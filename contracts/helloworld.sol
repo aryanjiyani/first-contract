bea90a355b44b609d7d48c97e5cff4b3d3207031
Aryan Jiyani <91625519+aryanjiyani@users.noreply.github.com>
2023-02-21 15:23:15 +0530

    Update helloworld.sol

--- contracts/helloworld.sol
+++ contracts/helloworld.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.0;
 
 contract helloworld {
     function hello() public pure returns (string memory) {
-        return "Hello, World";
+        return "Hello Hardhat";
     }
 }
